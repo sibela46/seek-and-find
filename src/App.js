84cd849c43c5ebdc983b82a6391ebf86ce4ca36d
sibela46 <sibelachinareva@gmail.com>
2018-04-24 23:09:21 +0100

    trying with my website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import brain from './img/brain.png';
 import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hello
+        <header className="App-header">
+          <img className="logo" src = {brain} width = "80" alt = "brain"/>
+          <h1 className="App-title">Търси и ще намериш.</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
