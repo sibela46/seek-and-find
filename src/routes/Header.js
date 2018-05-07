e3e01661f00f19f841676616585d1d57f0977f0f
sibela46 <sibelachinareva@gmail.com>
2018-05-07 18:56:47 +0100

    routes fixed

--- src/routes/Header.js
+++ src/routes/Header.js
@@ -4,21 +4,18 @@ import './css/App.css';
 import { Link } from "react-router-dom";
 
 var start = true;
+var lastActive = "";
 
 export default class Header extends React.Component {
   componentDidMount(){
-    var startPath = window.location.pathname;
-    document.getElementsByClassName(startPath)[0].id += "start-active";
+    lastActive = window.location.pathname;
+    document.getElementsByClassName(lastActive)[0].id = "active";
   }
   toggleClass(event) {
-    if (start){
-      document.getElementById("start-active").id = "";
-      start = false;
-    }
-    else{
-      document.getElementById("active").id = "";
-    }
-    event.target.id += "active";
+    var newPath = "/" + event.target.className;
+    lastActive = window.location.pathname;
+    document.getElementsByClassName(lastActive)[0].id = "";
+    document.getElementsByClassName(newPath)[0].id = "active";
   }
   render() {
     return (
@@ -26,9 +23,9 @@ export default class Header extends React.Component {
         <Link to = '/seek-and-find'><img className="logo" src = {brain} width = "80" alt = "brain"/></Link>
         <h1 className="App-title">Търси и ще намериш.</h1>
         <div className = "routes">
-          <Link to = '/about' className = "/about" onClick = {this.toggleClass}><h2>За мен</h2></Link>
-          <Link to = '/places' className = "/places" onClick={this.toggleClass}><h2>Места</h2></Link>
-          <Link to = '/seek-and-find' className = "/seek-and-find" onClick={this.toggleClass}><h2>Начало</h2></Link>
+          <Link to = '/about' className = "/about" onClick = {this.toggleClass}><h2 className = "about">За мен</h2></Link>
+          <Link to = '/places' className = "/places" onClick={this.toggleClass}><h2 className = "places">Места</h2></Link>
+          <Link to = '/seek-and-find' className = "/seek-and-find" onClick={this.toggleClass}><h2 className = "seek-and-find">Начало</h2></Link>
         </div>
       </header>
     );
