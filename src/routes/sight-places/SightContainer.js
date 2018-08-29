7d29ef3fb15d6b5d06c67b1ede79230699860004
sibela46 <sibelachinareva@gmail.com>
2018-08-29 14:16:32 +0300

    Homepage animation added

--- src/routes/sight-places/SightContainer.js
+++ src/routes/sight-places/SightContainer.js
@@ -8,7 +8,7 @@ class SeaContainer extends React.Component{
     return (
       <div>
         <h1>{name}</h1>
-        <img key = {image} src = {require(`../sight-places/sight-img/${image}`)} width = "500" height = "300" alt = ""/>
+        <img className="single-place-image" key = {image} src = {require(`../sight-places/sight-img/${image}`)} width = "500" height = "300" alt = ""/>
         <p>{description}</p>
       </div>
     )
