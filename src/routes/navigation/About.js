e5b3a13e927d7a28f7dc8d2a512c875872d41c4e
sibela46 <sibelachinareva@gmail.com>
2018-08-29 14:33:53 +0300

    Homepage animation added

--- src/routes/navigation/About.js
+++ src/routes/navigation/About.js
@@ -20,34 +20,34 @@ export default class About extends React.Component {
   }
   render () {
     return (
-    <div>
-    <div className ="container">
-      <p>Centered!</p>
-    </div>
-    <div id = "slides" style = {{width: '300px', height: '100px'}}>
-      <div className = "slide" style = {{display: 'inline-block', background: '#21A0A0', width: '200px', height: '200px'}}>
-        <h2>Hello</h2>
+    <div className="about-container">
+      <div className ="container">
+        <p>Centered!</p>
       </div>
-      <div className = "slide" style = {{background: '#EDF67D', width: '300px', height: '100px'}}>
-        <h2>there,</h2>
+      <div id = "slides" style = {{width: '300px', height: '100px'}}>
+        <div className = "slide" style = {{display: 'inline-block', background: '#21A0A0', width: '200px', height: '200px'}}>
+          <h2>Hello</h2>
+        </div>
+        <div className = "slide" style = {{background: '#EDF67D', width: '300px', height: '100px'}}>
+          <h2>there,</h2>
+        </div>
+        <div className = "slide" style = {{background: '#F896D8', width: '200px', height: '200px'}}>
+          <h2>friend!</h2>
+        </div>
+        <div className = "slide" style = {{background: '#CA7DF9', width: '100px', height: '100px'}}>
+          <h2>How</h2>
+        </div>
+        <div className = "slide" style = {{background: '#724CF9', width: '300px', height: '200px'}}>
+          <h2>are you</h2>
+        </div>
+        <div className = "slide" style = {{background: '#564592', width: '100px', height: '200px'}}>
+          <h2>today?</h2>
+        </div>
       </div>
-      <div className = "slide" style = {{background: '#F896D8', width: '200px', height: '200px'}}>
-        <h2>friend!</h2>
+      <div className = "buttons">
+        <button className = "prev" onClick = {() => this.changeSlide(-1)}>Prev</button>
+        <button className = "next" onClick = {() => this.changeSlide(+1)}>Next</button>
       </div>
-      <div className = "slide" style = {{background: '#CA7DF9', width: '100px', height: '100px'}}>
-        <h2>How</h2>
-      </div>
-      <div className = "slide" style = {{background: '#724CF9', width: '300px', height: '200px'}}>
-        <h2>are you</h2>
-      </div>
-      <div className = "slide" style = {{background: '#564592', width: '100px', height: '200px'}}>
-        <h2>today?</h2>
-      </div>
-    </div>
-    <div className = "buttons">
-      <button className = "prev" onClick = {() => this.changeSlide(-1)}>Prev</button>
-      <button className = "next" onClick = {() => this.changeSlide(+1)}>Next</button>
-    </div>
     </div>
    );
  }
